7c8542455625962fde7cf4ab0547f702db982e38
Markus Ylisiurunen <markus.ylisiurunen@gmail.com>
2019-01-08 22:11:16 +0200

    Add util module for components

---
+++ src/utils/components.js
@@ -0,0 +1,13 @@
+/**
+ * @overview Component utility functions.
+ */
+
+/**
+ * Convert a prop to either pixel or percentage value.
+ * @param {Mixed}  value    Either a number for pixels or string to be returned as-is.
+ * @param {String} fallback Value to fall back to if no value provided.
+ */
+export const toPixelOrPercentage = (value, fallback) => {
+  if (value === undefined || value === null) return fallback;
+  return typeof value === 'string' ? value : `${value}px`;
+};
