56c13f63a2bc28f08d4faf647290d26f821fbe72
Markus Ylisiurunen <markus.ylisiurunen@gmail.com>
2019-01-09 13:37:41 +0200

    Modify the front page component to use the new components

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,9 @@
 
 import React from 'react';
 import Helmet from 'react-helmet';
-import { Link, graphql } from 'gatsby';
 
+import State from '../components/State';
+import Blur from '../components/Blur';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
 import Layout from '../components/Layout';
@@ -13,16 +14,18 @@ import Layout from '../components/Layout';
 class FrontPage extends React.Component {
   render() {
     return (
-      <React.Fragment>
+      <State.Provider>
         <Helmet>
           <link
             rel="stylesheet"
             href="https://maxcdn.icons8.com/fonts/line-awesome/1.1/css/line-awesome-font-awesome.min.css"
           />
         </Helmet>
-        <Header />
-        <Hero />
-      </React.Fragment>
+        <Blur>
+          <Header />
+          <Hero />
+        </Blur>
+      </State.Provider>
     );
   }
 }
