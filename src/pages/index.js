f810e7120fd123e190ade307a2a1539e2bb8a021
Markus Ylisiurunen <markus.ylisiurunen@gmail.com>
2019-01-09 13:57:08 +0200

    Inject the overlay menu to the front page

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ import Helmet from 'react-helmet';
 
 import State from '../components/State';
 import Blur from '../components/Blur';
+import Menu from '../components/Menu';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
 import Layout from '../components/Layout';
@@ -21,6 +22,7 @@ class FrontPage extends React.Component {
             href="https://maxcdn.icons8.com/fonts/line-awesome/1.1/css/line-awesome-font-awesome.min.css"
           />
         </Helmet>
+        <Menu />
         <Blur>
           <Header />
           <Hero />
