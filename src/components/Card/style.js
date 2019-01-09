999986920eb17c344f4ef89412d402f04302bdf9
Markus Ylisiurunen <markus.ylisiurunen@gmail.com>
2019-01-09 21:07:14 +0200

    Update the card component's props

--- src/components/Card/style.js
+++ src/components/Card/style.js
@@ -4,17 +4,14 @@
 
 import styled from 'styled-components';
 
+// prettier-ignore
 const Container = styled.div`
   border-radius: 6px;
-  border: ${p => (p.border ? '1px solid rgba(0, 0, 0, 0.08)' : 'none')};
-  margin-bottom: ${p => (p.noMargin ? '0' : '16px')};
-  padding: ${p => (p.noPadding ? '0' : '12px')};
+  border: ${p => (p.border === undefined ? '1px solid rgba(0, 0, 0, 0.08)' : p.border)};
+  margin-bottom: ${p => (p.marginBottom === undefined ? '16px' : p.marginBottom)};
+  padding: ${p => (p.padding === undefined ? '12px' : p.padding)};
   position: relative;
   width: 100%;
-
-  &:last-child {
-    margin-bottom: 0;
-  }
 `;
 
 export default { Container };
