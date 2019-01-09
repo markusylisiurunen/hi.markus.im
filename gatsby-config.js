450213a2ce6ddd4cffb86dc0e1fb7c38afeafe09
Markus Ylisiurunen <markus.ylisiurunen@gmail.com>
2019-01-09 19:49:33 +0200

    Use normal quotes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,63 +1,63 @@
 module.exports = {
   siteMetadata: {
     title: 'Markus Ylisiurunen',
-    author: `Markus Ylisiurunen`,
-    description: `Markus Ylisiurunen\'s personal website.`,
-    siteUrl: `https://markus.im/`,
+    author: 'Markus Ylisiurunen',
+    description: "Markus Ylisiurunen's personal website.",
+    siteUrl: 'https://markus.im/',
   },
   plugins: [
-    'gatsby-plugin-styled-components',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/content/blog`,
-        name: `blog`,
+        name: 'blog',
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/content/assets`,
-        name: `assets`,
+        name: 'assets',
       },
     },
     {
-      resolve: `gatsby-transformer-remark`,
+      resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
           {
-            resolve: `gatsby-remark-images`,
+            resolve: 'gatsby-remark-images',
             options: {
               maxWidth: 590,
             },
           },
           {
-            resolve: `gatsby-remark-responsive-iframe`,
+            resolve: 'gatsby-remark-responsive-iframe',
             options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
+              wrapperStyle: 'margin-bottom: 1.0725rem',
             },
           },
-          `gatsby-remark-prismjs`,
-          `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-smartypants`,
+          'gatsby-remark-prismjs',
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-smartypants',
         ],
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: 'gatsby-plugin-google-analytics',
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        //trackingId: 'ADD YOUR TRACKING ID HERE',
       },
     },
-    `gatsby-plugin-feed`,
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-feed',
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-plugin-typography`,
+      resolve: 'gatsby-plugin-typography',
       options: {
-        pathToConfigModule: `src/utils/typography`,
+        pathToConfigModule: 'src/utils/typography',
       },
     },
+    'gatsby-plugin-styled-components',
   ],
 };
