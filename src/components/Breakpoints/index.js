08e42e79509579e8268fd7431dff87337f8844b3
Markus Ylisiurunen <markus.ylisiurunen@gmail.com>
2019-01-09 22:34:32 +0200

    Add breakpoints component

---
+++ src/components/Breakpoints/index.js
@@ -0,0 +1,59 @@
+/**
+ * @overview Breakpoints component.
+ */
+
+import React from 'react';
+
+const BreakpointsContext = React.createContext({});
+
+class Provider extends React.Component {
+  state = {
+    currentBreakpoint: null,
+  };
+
+  handleResize = () => {
+    const windowWidth = window.innerWidth;
+    const { breakpoints } = this.props;
+    const { currentBreakpoint } = this.state;
+
+    const breakpointNames = Object.keys(breakpoints);
+    let newBreakpoint = null;
+
+    for (let i = 0; i < breakpointNames.length; i += 1) {
+      const bpName = breakpointNames[i];
+      const bpWidth = breakpoints[bpName];
+
+      if (bpWidth > windowWidth) continue;
+
+      if (newBreakpoint === null || breakpoints[newBreakpoint] < bpWidth) {
+        newBreakpoint = bpName;
+      }
+    }
+
+    if (currentBreakpoint !== newBreakpoint) {
+      this.setState({ currentBreakpoint: newBreakpoint });
+    }
+  };
+
+  componentWillMount() {
+    this.handleResize();
+    window.addEventListener('resize', this.handleResize, { passive: true });
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleResize);
+  }
+
+  render() {
+    const { breakpoints } = this.props;
+    const { currentBreakpoint } = this.state;
+
+    return (
+      <BreakpointsContext.Provider value={{ breakpoints, currentBreakpoint }}>
+        {this.props.children}
+      </BreakpointsContext.Provider>
+    );
+  }
+}
+
+export default { Provider, Consumer: BreakpointsContext.Consumer };
