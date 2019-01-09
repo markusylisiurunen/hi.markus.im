e85a0d2fbd545b09d4e8df1a3e1e0df710df1c41
Markus Ylisiurunen <markus.ylisiurunen@gmail.com>
2019-01-09 22:20:33 +0200

    Use babel-eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,11 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es6": true,
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    es6: true,
   },
-  "plugins": [
-    "react",
-  ],
-  "globals": {
-    "graphql": false,
+  plugins: ['react'],
+  globals: {
+    graphql: false,
   },
-  "parserOptions": {
-    "sourceType": "module",
-    "ecmaFeatures": {
-      "experimentalObjectRestSpread": true,
-      "jsx": true,
-    },
-  }
-}
+};
