345ea3698a98f249d6022354646cb8d8664ea9ac
Markus Ylisiurunen <markus.ylisiurunen@gmail.com>
2019-01-29 23:54:17 +0200

    Add colors util

---
+++ src/utils/colors.js
@@ -0,0 +1,15 @@
+export default {
+  accentPink: 'hsl(336, 94%, 65%)',
+
+  brandGmail: 'hsl(342, 54%, 50%)',
+  brandLinkedin: 'hsl(201, 100%, 35%)',
+  brandGithub: 'hsl(0, 5%, 9%)',
+
+  fileTypePdf: 'hsl(0, 91%, 40%)',
+
+  textHard: 'hsl(0, 0%, 12%)',
+  textSoft: 'hsl(0, 0%, 60%)',
+
+  borderHard: 'hsl(0, 0%, 82%)',
+  borderSoft: 'hsl(0, 0%, 90%)',
+}
