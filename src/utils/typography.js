2cb6d6f9aaad7fec69e1f9b9b5d12213fc029e1c
Markus Ylisiurunen <markus.ylisiurunen@gmail.com>
2019-01-29 22:18:29 +0200

    Minor updates

---
+++ src/utils/typography.js
@@ -0,0 +1,26 @@
+import Typography from 'typography'
+
+const typography = new Typography({
+  baseFontSize: 16,
+  headerFontFamily: ['Raleway', 'sans-serif'],
+  bodyFontFamily: ['Montserrat', 'serif'],
+  headerGray: 12,
+  bodyGray: 12,
+  googleFonts: [
+    {
+      name: 'Raleway',
+      styles: ['400', '500', '600', '700'],
+    },
+    {
+      name: 'Montserrat',
+      styles: ['400', '500', '600', '700'],
+    },
+  ],
+})
+
+// Hot reload typography in development.
+if (process.env.NODE_ENV !== `production`) {
+  typography.injectStyles()
+}
+
+export default typography
