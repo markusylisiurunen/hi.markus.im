39afe921a8ba3f9c10a907512cbfd799a71e5f36
Markus Ylisiurunen <markus.ylisiurunen@gmail.com>
2019-01-29 23:54:09 +0200

    Add an education component

---
+++ src/components/Education/index.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Container, School, Degree, Dates, Description } from './style'
+import Attachment from '../Attachment'
+
+const Education = ({
+  school,
+  degree,
+  startDate,
+  endDate,
+  description,
+  attachments,
+}) => (
+  <Container>
+    <School>{school}</School>
+    <Degree>{degree}</Degree>
+    <Dates>{`${startDate} - ${endDate}`}</Dates>
+    <div>
+      {description.map(section => (
+        <Description key={section}>{section}</Description>
+      ))}
+    </div>
+    {Array.isArray(attachments) && attachments.length && (
+      <div
+        css={`
+          padding-top: 24px;
+        `}
+      >
+        {attachments.map(attachment => (
+          <Attachment {...attachment} key={attachment.name} />
+        ))}
+      </div>
+    )}
+  </Container>
+)
+
+export default Education
---
+++ src/components/Education/style.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components'
+import colors from '../../utils/colors'
+
+export const Container = styled.div`
+  margin-bottom: 32px;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+`
+
+export const School = styled.h3`
+  font-size: 1em;
+  font-weight: 700;
+  line-height: inherit;
+  margin-bottom: 4px;
+  margin-top: 0;
+`
+
+export const Degree = styled.p`
+  margin-bottom: 4px;
+  margin-top: 0;
+`
+
+export const Dates = styled.p`
+  color: ${colors.textSoft};
+  margin-bottom: 16px;
+  margin-top: 0;
+`
+
+export const Description = styled.p`
+  &:last-child {
+    margin-bottom: 0;
+  }
+`
