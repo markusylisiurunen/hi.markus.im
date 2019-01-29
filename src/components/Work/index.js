3e2c790aca30c64f40832603a5278e98ee990b96
Markus Ylisiurunen <markus.ylisiurunen@gmail.com>
2019-01-29 23:57:43 +0200

    Add Work component

---
+++ src/components/Work/index.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Container, Employer, JobTitle, Dates, Description } from './style'
+import Attachment from '../Attachment'
+
+const Work = ({
+  employer,
+  jobTitle,
+  startDate,
+  endDate,
+  description,
+  attachments,
+}) => (
+  <Container>
+    <Employer>{employer}</Employer>
+    <JobTitle>{jobTitle}</JobTitle>
+    <Dates>{`${startDate} - ${endDate}`}</Dates>
+    <div>
+      {description.map(section => (
+        <Description key={section}>{section}</Description>
+      ))}
+    </div>
+    {Array.isArray(attachments) && attachments.length && (
+      <div
+        css={`
+          padding-top: 24px;
+        `}
+      >
+        {attachments.map(attachment => (
+          <Attachment {...attachment} key={attachment.name} />
+        ))}
+      </div>
+    )}
+  </Container>
+)
+
+export default Work
