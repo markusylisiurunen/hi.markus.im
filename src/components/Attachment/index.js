c0ef09c9c0ea80c2e3b03c90311994e3c5bd8f08
Markus Ylisiurunen <markus.ylisiurunen@gmail.com>
2019-01-30 23:31:42 +0200

    Update the Attachment component to properly add the href for the attachment

--- src/components/Attachment/index.js
+++ src/components/Attachment/index.js
@@ -1,12 +1,39 @@
 import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
 import { mapFileTypeToIcon } from './utils'
 import { Container, FileTypeIcon } from './style'
 
-const Attachment = ({ type, name, href }) => (
-  <Container href={href || false} download>
-    <FileTypeIcon className={`fa ${mapFileTypeToIcon(type)}`} />
-    <span>{`${name} (.${type})`}</span>
-  </Container>
+const Attachment = ({ type, name, fileName }) => (
+  <StaticQuery
+    query={graphql`
+      query AttachmentQuery {
+        allFile(filter: { sourceInstanceName: { eq: "attachments" } }) {
+          edges {
+            node {
+              name
+              extension
+              publicURL
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <Container
+        href={
+          data.allFile.edges
+            .map(({ node }) => node)
+            .find(
+              ({ name, extension }) => extension === type && name === fileName
+            ).publicURL
+        }
+        download
+      >
+        <FileTypeIcon className={`fa ${mapFileTypeToIcon(type)}`} />
+        <span>{`${name} (.${type})`}</span>
+      </Container>
+    )}
+  />
 )
 
 export default Attachment
