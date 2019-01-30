ddd36d067dca6717528584d1c2b505aae8aebd8b
Markus Ylisiurunen <markus.ylisiurunen@gmail.com>
2019-01-30 23:19:14 +0200

    Add a test attachment pdf

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,13 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
+        name: `attachments`,
+        path: `${__dirname}/attachments`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
