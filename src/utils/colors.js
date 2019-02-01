435a7ac2e77a62a6d6ed20c4a7c21660d55849d1
Markus Ylisiurunen <markus.ylisiurunen@gmail.com>
2019-02-01 23:36:57 +0200

    Update colors

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,15 +1,29 @@
 export default {
+  // Accent colors used throughout the interface
   accentPink: 'hsl(336, 94%, 65%)',
 
+  // External service brand colors
+  brandGithub: 'hsl(0, 5%, 9%)',
+  brandGitlab: 'hsl(20, 97%, 57%)',
   brandGmail: 'hsl(342, 54%, 50%)',
   brandLinkedin: 'hsl(201, 100%, 35%)',
-  brandGithub: 'hsl(0, 5%, 9%)',
+  brandTelegram: 'hsl(200, 100%, 40%)',
 
+  // File types
   fileTypePdf: 'hsl(0, 91%, 40%)',
 
+  // Background colors
+  backgroundBlack: 'hsl(210, 8%, 16%)',
+
+  // Text colors
   textHard: 'hsl(0, 0%, 12%)',
   textSoft: 'hsl(0, 0%, 60%)',
+  textWhite: 'hsl(0, 0%, 96%)',
 
+  // Border colors
   borderHard: 'hsl(0, 0%, 82%)',
   borderSoft: 'hsl(0, 0%, 90%)',
+
+  // Shadow colors
+  shadowSoft: 'hsla(210, 16%, 10%, 0.14)',
 }
