284d3eb2a254a18c0c29faf179564fab3c409c75
Markus Ylisiurunen <markus.ylisiurunen@gmail.com>
2019-02-02 00:54:36 +0200

    Make spacer component responsive

--- src/components/Grid/style.js
+++ src/components/Grid/style.js
@@ -3,9 +3,9 @@ import styled from 'styled-components'
 // TODO: These breakpoints could be dynamic
 const breakpoints = { md: 768, lg: 1025 }
 
-const width = span => {
-  return `width: ${(parseInt(span, 10) / 12) * 100}%;`
-}
+const height = _ => `height: ${parseInt(_, 10)}px;`
+
+const width = _ => `width: ${(parseInt(_, 10) / 12) * 100}%;`
 
 export const StyledContainer = styled.div`
   margin: 0 auto;
@@ -35,6 +35,14 @@ export const StyledColumn = styled.div`
 `
 
 export const StyledSpacer = styled.div`
-  height: ${({ height }) => parseInt(height)}px;
   width: 100%;
+  ${({ heightXs }) => heightXs && height(heightXs || 0)}
+
+  @media screen and (min-width: ${breakpoints.md}px) {
+    ${({ heightMd }) => heightMd && height(heightMd || 0)}
+  }
+
+  @media screen and (min-width: ${breakpoints.lg}px) {
+    ${({ heightLg }) => heightLg && height(heightLg || 0)}
+  }
 `
