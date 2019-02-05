c067590d2bbc6a196dca24eb04711d27374d00c4
Markus Ylisiurunen <markus.ylisiurunen@gmail.com>
2019-02-05 21:21:16 +0200

    Add Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,21 +37,11 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: `gatsby-starter-default`,
-    //     short_name: `starter`,
-    //     start_url: `/`,
-    //     background_color: `#663399`,
-    //     theme_color: `#663399`,
-    //     display: `minimal-ui`,
-    //     icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-    //   },
-    // },
-    //
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `UA-133876555-1`,
+      },
+    },
   ],
 }
