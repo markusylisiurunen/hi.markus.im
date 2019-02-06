dd3ee45813f18943fd2d6affdb6260a24f9b9911
Markus Ylisiurunen <markus.ylisiurunen@gmail.com>
2019-02-06 20:17:43 +0200

    Update the logo

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -5,14 +5,13 @@ import colors from '../../utils/colors'
 const Svg = styled.svg`
   fill: ${colors.textHard};
   height: ${p => parseInt(p.height, 10)}px;
-  width: ${p => 1.15 * parseInt(p.height, 10)}px;
 `
 
 const Logo = props => (
-  <Svg viewBox="0 0 115 100" xmlns="http://www.w3.org/2000/svg" {...props}>
-    <rect x="0" y="0" width="33" height="100" />
-    <rect x="41" y="0" width="33" height="65" />
-    <rect x="82" y="0" width="33" height="100" />
+  <Svg viewBox="0 0 417 356" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <rect x="8" y="8" width="120" height="340" />
+    <path d="M409 348H269L409 148V348Z" />
+    <path d="M409 8H287L193.5 134.5L128 8H116L234 247L409 8Z" />
   </Svg>
 )
 
